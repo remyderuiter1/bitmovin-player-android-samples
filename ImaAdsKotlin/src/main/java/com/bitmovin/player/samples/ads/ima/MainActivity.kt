7c238d2d0591c9e7b040f74a6a26b26a3139e91e
remyderuiter <r.deruiter@wearetriple.com>
2024-04-23 11:38:24 +0200

    Testing example no continue after midroll skip

--- ImaAdsKotlin/src/main/java/com/bitmovin/player/samples/ads/ima/MainActivity.kt
+++ ImaAdsKotlin/src/main/java/com/bitmovin/player/samples/ads/ima/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.bitmovin.player.samples.ads.ima
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.bitmovin.analytics.api.AnalyticsConfig
@@ -13,6 +14,8 @@ import com.bitmovin.player.api.advertising.AdSourceType
 import com.bitmovin.player.api.advertising.AdvertisingConfig
 import com.bitmovin.player.api.analytics.AnalyticsPlayerConfig
 import com.bitmovin.player.api.source.SourceConfig
+import com.bitmovin.player.api.source.SourceOptions
+import com.bitmovin.player.api.source.TimelineReferencePoint
 import com.bitmovin.player.samples.ads.ima.databinding.ActivityMainBinding
 
 // These are IMA Sample Tags from https://developers.google.com/interactive-media-ads/docs/sdks/android/tags
@@ -46,11 +49,39 @@ class MainActivity : AppCompatActivity() {
         // Set up a post-roll ad
         val postRoll = AdItem("post", fourthAdSource)
 
+        val sourceConfig = SourceConfig.fromUrl("https://bitdash-a.akamaihd.net/content/sintel/sintel.mpd")
+        sourceConfig.options = SourceOptions(600.0, TimelineReferencePoint.Start)
+
+        val vMapUrlNlziet =
+            "https://7b936.v.fwmrm.net/ad/g/1?caid=6kJ8xWlwVXZ&csid=NLZIETAndroidExoPlayer&vdur=2194.3200683&pvrn=432932&vprn=700853&_fw_gdpr=0&metr=1031&resp=vmap1&nw=506166&prof=506166%3Asanoma_sbs_external_live&flag=%2Bfbad%2Bemcr%2Bslcb%2Bsltp%2Bamcb%2Bplay;talpa_consent%3D0"
+        val adsNlziet = AdItem(
+            AdSource(
+                AdSourceType.Ima,
+                vMapUrlNlziet
+            )
+        )
+        val vMapUrlTest =
+            "https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/vmap_ad_samples&sz=640x480&cust_params=sample_ar%3Dpremidpostlongpod&ciu_szs=300x250&gdfp_req=1&ad_rule=1&output=vmap&unviewed_position_start=1&env=vp&impl=s&cmsid=496&vid=short_onecue&correlator="
+        val adsSample = AdItem(AdSource(AdSourceType.Ima, vMapUrlTest))
+
         // Add the AdItems to the AdvertisingConfig
-        val advertisingConfig = AdvertisingConfig(preRoll, midRoll, postRoll)
+        val advertisingConfig = AdvertisingConfig(
+            schedule = listOf(adsSample),
+            adsManagerAvailableCallback = { adsManager ->
+                adsManager.addAdEventListener {
+                    Log.e("testing", "Event: $it")
+                }
+            },
+            shouldPlayAdBreak = { adBreak ->
+                Log.e("testing", "Checking shouldPlayAd")
+                false
+            },
+        )
 
         // Create a new PlayerConfig containing the advertising config. Ads in the AdvertisingConfig will be scheduled automatically.
-        val playerConfig = PlayerConfig(advertisingConfig = advertisingConfig)
+        val playerConfig = PlayerConfig(
+            advertisingConfig = advertisingConfig
+        )
 
         // Create new Player with our PlayerConfig
         val analyticsKey = "{ANALYTICS_LICENSE_KEY}"
@@ -59,14 +90,17 @@ class MainActivity : AppCompatActivity() {
             playerConfig,
             AnalyticsPlayerConfig.Enabled(AnalyticsConfig(analyticsKey)),
         )
-        playerView = PlayerView(this, player).apply {
+        playerView = PlayerView(
+            this, player,
+        ).apply {
             layoutParams = LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.MATCH_PARENT
             )
         }
         playerView.keepScreenOn = true
-        player.load(SourceConfig.fromUrl("https://bitdash-a.akamaihd.net/content/sintel/sintel.mpd"))
+        player.load(sourceConfig)
+        player.play()
 
         // Add PlayerView to the layout
         binding.root.addView(playerView, 0)
