ff4233af71e25baf52b4663a32061d20133ea36b
Mirko <m.volkers@wearetriple.com>
2024-05-01 16:22:12 +0200

    Reproducing player sometimes hanging while trying to play ad

--- ImaAdsKotlin/src/main/java/com/bitmovin/player/samples/ads/ima/MainActivity.kt
+++ ImaAdsKotlin/src/main/java/com/bitmovin/player/samples/ads/ima/MainActivity.kt
@@ -5,6 +5,7 @@ import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.bitmovin.analytics.api.AnalyticsConfig
 import com.bitmovin.player.PlayerView
+import com.bitmovin.player.api.PlaybackConfig
 import com.bitmovin.player.api.Player
 import com.bitmovin.player.api.PlayerConfig
 import com.bitmovin.player.api.advertising.AdItem
@@ -14,12 +15,10 @@ import com.bitmovin.player.api.advertising.AdvertisingConfig
 import com.bitmovin.player.api.analytics.AnalyticsPlayerConfig
 import com.bitmovin.player.api.source.SourceConfig
 import com.bitmovin.player.samples.ads.ima.databinding.ActivityMainBinding
+import com.google.ads.interactivemedia.v3.api.AdEvent
 
 // These are IMA Sample Tags from https://developers.google.com/interactive-media-ads/docs/sdks/android/tags
-private const val AD_SOURCE_1 = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dredirecterror&nofb=1&correlator="
-private const val AD_SOURCE_2 = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator="
-private const val AD_SOURCE_3 = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator="
-private const val AD_SOURCE_4 = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dredirectlinear&correlator="
+val vmap = "https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/vmap_ad_samples&sz=640x480&cust_params=sample_ar%3Dpremidpostlongpod&ciu_szs=300x250&gdfp_req=1&ad_rule=1&output=vmap&unviewed_position_start=1&env=vp&impl=s&cmsid=496&vid=short_onecue&correlator="
 
 class MainActivity : AppCompatActivity() {
     private lateinit var playerView: PlayerView
@@ -30,31 +29,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Create AdSources
-        val firstAdSource = AdSource(AdSourceType.Ima, AD_SOURCE_1)
-        val secondAdSource = AdSource(AdSourceType.Ima, AD_SOURCE_2)
-        val thirdAdSource = AdSource(AdSourceType.Ima, AD_SOURCE_3)
-        val fourthAdSource = AdSource(AdSourceType.Ima, AD_SOURCE_4)
-
-        // Set up a pre-roll ad
-        val preRoll = AdItem("pre", thirdAdSource)
-
-        // Set up a mid-roll waterfalling ad at 10% of the content duration
-        // NOTE: AdItems containing more than one AdSource, will be executed as waterfalling ad
-        val midRoll = AdItem("10%", firstAdSource, secondAdSource)
-
-        // Set up a post-roll ad
-        val postRoll = AdItem("post", fourthAdSource)
-
         // Add the AdItems to the AdvertisingConfig
-        val advertisingConfig = AdvertisingConfig(preRoll, midRoll, postRoll)
+        val advertisingConfig = AdvertisingConfig(listOf(AdItem(AdSource(AdSourceType.Ima, vmap))))
 
         // Create a new PlayerConfig containing the advertising config. Ads in the AdvertisingConfig will be scheduled automatically.
-        val playerConfig = PlayerConfig(advertisingConfig = advertisingConfig)
+        val playerConfig = PlayerConfig(advertisingConfig = advertisingConfig, playbackConfig = PlaybackConfig(isAutoplayEnabled = true))
 
         // Create new Player with our PlayerConfig
         val analyticsKey = "{ANALYTICS_LICENSE_KEY}"
-        val player = Player(
+       val player = Player(
             this,
             playerConfig,
             AnalyticsPlayerConfig.Enabled(AnalyticsConfig(analyticsKey)),
@@ -66,10 +49,11 @@ class MainActivity : AppCompatActivity() {
             )
         }
         playerView.keepScreenOn = true
-        player.load(SourceConfig.fromUrl("https://bitdash-a.akamaihd.net/content/sintel/sintel.mpd"))
-
         // Add PlayerView to the layout
         binding.root.addView(playerView, 0)
+
+        player.load(SourceConfig.fromUrl("https://bitdash-a.akamaihd.net/content/sintel/sintel.mpd"))
+
     }
 
     override fun onStart() {
